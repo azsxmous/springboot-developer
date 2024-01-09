29cf4c234fb337975a50f525885e187077f42df3
azsxmous <azsxmous@gmail.com>
2024-01-09 23:00:44 +0900

    JWT 토큰 인증 유효성 검증 추가

--- src/main/java/org/example/springbootdeveloper/config/jwt/TokenProvider.java
+++ src/main/java/org/example/springbootdeveloper/config/jwt/TokenProvider.java
@@ -1,15 +1,21 @@
 package org.example.springbootdeveloper.config.jwt;
 
+import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Header;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import lombok.RequiredArgsConstructor;
 import org.example.springbootdeveloper.domain.User;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.stereotype.Service;
 
 
 import java.time.Duration;
+import java.util.Collections;
 import java.util.Date;
+import java.util.Set;
 
 @RequiredArgsConstructor
 @Service
@@ -38,5 +44,22 @@ public class TokenProvider {
                 .signWith(SignatureAlgorithm.HS256, jwtProperties.getSecretKey())
                 .compact();
     }
+    // JWT 토큰 유효성 검증 메서드
+    public boolean validToken(String token) {
+        try {
+            Jwts.parser()
+                    .setSigningKey(jwtProperties.getSecretKey()) // 비밀값으로 복호화
+                    .parseClaimsJws(token);
+            return true;
+        } catch (Exception e) { // 복호화 과정에서 에러가 발생하면 유효하지 않은 토큰
+            return false;
+        }
+    }
 
+    // 토큰 기반으로 인증 정보를 가져오는 메서드
+    public Authentication getAuthentication (String token) {
+        Claims claims = getClaims(token);
+        Set<SimpleGrantedAuthority> authorities = Collections.singleton(new SimpleGrantedAuthority("ROLE_USER"));
+        return new UsernamePasswordAuthenticationToken(new org.springframework.security.core.userdetails.User(claims.getSubject(), "", authorities), token, authorities);
+    }
 }
